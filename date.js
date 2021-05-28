14a71dff6e7160e42235154c63b934c20584936c
zsda111 <>
2021-05-28 15:13:59 +0000

    only manage to get half of it done

--- date.js
+++ date.js
@@ -1,5 +1,17 @@
-function getDate() 
-{
+   function getDate() 
+  {
 	var dateString = new Date();
 	document.getElementById("date").innerHTML=dateString;
-}
+  }
+   function hours()
+  {
+   document.getElementById("hours").style.color="orange";
+  }
+   function minutes()
+  {
+   document.getElementById("minutes").style.color="red";
+  }
+   function date/time()
+   {
+   document.getElementById("date").innerHTML="Date/time";
+   }
